badc5ad3f8315a9ed861071af8690ee781a1310b
Xavier BIZOUX <xavier.bizouxsas.com>
2020-04-30 17:38:53 +0200

    Fix alignment in updateReportContent.py

--- viyaRestPy/Reports/updateReportContent.py
+++ viyaRestPy/Reports/updateReportContent.py
@@ -10,9 +10,9 @@ def updateReportContent(content="", report={}, name="", path="", auth={}):
         if report != {}:
             endpoint = "/reports/reports/{0:s}/content".format(
             report["json"]["id"])
-            data = content
-            headers = {
-                'Content-Type': 'application/vnd.sas.report.content+json',
-                'If-Match': report["headers"]["ETag"]
-            }
-            callRest(endpoint, "put", data=data, headers=headers, auth=auth)
+    data = content
+    headers = {
+        'Content-Type': 'application/vnd.sas.report.content+json',
+        'If-Match': report["headers"]["ETag"]
+    }
+    callRest(endpoint, "put", data=data, headers=headers, auth=auth)
